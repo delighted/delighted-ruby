a7c9a9d4a8bc4f07bc3b1aa560a23d2b05f7497f
Mark Dodwell <mark@mkdynamic.co.uk>
2014-02-11 23:11:02 -0800

    enforce required argument

--- lib/delighted/resources/survey_request.rb
+++ lib/delighted/resources/survey_request.rb
@@ -3,6 +3,7 @@ module Delighted
     self.path = "/people/{PERSON_EMAIL}/survey_requests"
 
     def self.delete_pending(attributes = {}, client = Delighted.shared_client)
+      raise ArgumentError, "You must pass :person_email" unless attributes[:person_email]
       interpolated_path = path.sub("{PERSON_EMAIL}", CGI.escape(attributes[:person_email]))
       interpolated_path << "/pending"
       json = client.delete_json(interpolated_path)
